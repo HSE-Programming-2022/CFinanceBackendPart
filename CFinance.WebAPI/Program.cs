a14ac0ae05e7782ffb6ba37339327a489d19d827
Nikolay Moroz <adedalmoroz@gmail.com>
2022-06-23 12:15:58 +0300

    fix context disposal

--- CFinance.WebAPI/Program.cs
+++ CFinance.WebAPI/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Urls.Add($"https://*:{port}");
+app.Urls.Add($"http://*:{port}");
 
 app.Run();
--- CFinance.WebAPI/Services/UserService.cs
+++ CFinance.WebAPI/Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using CFinance.Context;
 using CFinance.Context.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Any;
@@ -9,64 +10,74 @@ namespace CFinance.WebAPI.Services;
 public static class UserService
 {
     private static List<User> Users { get; set; }
-    private static List<Portfolio> portfolios { get; set; }
-    private static List< PortfolioCompany> companies { get; set; }
 
-    private static CFinanceDbContext AppContext { get; set; }
     static UserService()
     {
-        AppContext = new CFinanceDbContext();
-        
-        Users = AppContext.Users.ToList();
-        portfolios = AppContext.Portfolios.ToList();
-        companies = AppContext.PortfolioCompany.ToList();
     }
 
     public static List<User> GetAll() => Users;
 
     public static User? Get(int uid)
     {
-        return Users.FirstOrDefault(u => u.UserID == uid);
+        using (CFinanceDbContext appContext = new CFinanceDbContext())
+        {
+            Users = appContext.Users.ToList();
+            return Users.FirstOrDefault(u => u.UserID == uid);
+        }
     }
 
     public static User? Login(string username, string password)
     {
-        return Users.FirstOrDefault(u => (u.UserName == username) && (u.CheckPassword(password)));
+        using (CFinanceDbContext appContext = new CFinanceDbContext())
+        {
+            Users = appContext.Users.ToList();
+            return Users.FirstOrDefault(u => (u.UserName == username) && (u.CheckPassword(password)));
+        }
     }
     public static bool Add(User newUser)
     {
-        if (Users.Any(x => (x.UserName == newUser.UserName) || (x.Email == newUser.Email)))
+        using (CFinanceDbContext appContext = new CFinanceDbContext())
         {
-            return false;
-        }
+            Users = appContext.Users.ToList();
 
-        
-        newUser.HashPassword();
-        AppContext.Users.Add(newUser);
-        AppContext.SaveChanges();
+            if (Users.Any(x => (x.UserName == newUser.UserName) || (x.Email == newUser.Email)))
+            {
+                return false;
+            }
+
+
+            newUser.HashPassword();
+            appContext.Users.Add(newUser);
+            appContext.SaveChanges();
+        }
 
         return true;
     }
 
     public static void Delete(int uid)
     {
+        using (CFinanceDbContext appContext = new CFinanceDbContext())
+        {
+            var user = new User() { UserID = uid };
 
-        var user = new User() {UserID = uid};
+            appContext.Users.Attach(user);
+            appContext.Users.Remove(user);
 
-        AppContext.Users.Attach(user);
-        AppContext.Users.Remove(user);
-
-        AppContext.SaveChanges();
+            appContext.SaveChanges();
+        }
     }
 
     public static void Update(int uid)
     {
-        var user = new User() {UserID = uid};
+        using (CFinanceDbContext appContext = new CFinanceDbContext())
+        {
+            var user = new User() { UserID = uid };
 
-        AppContext.Users.Attach(user);
-        AppContext.Users.Update(user);
+            appContext.Users.Attach(user);
+            appContext.Users.Update(user);
 
-        AppContext.SaveChanges();
+            appContext.SaveChanges();
+        }
     }
 
 }
